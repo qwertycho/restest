8bc93674ebb1c3a1bbb5aab37a3fa96a3777d950
qwertycho <tychovopstal@gmail.com>
2022-12-07 13:08:45 +0100

    arguments

--- Program.cs
+++ Program.cs
@@ -2,8 +2,25 @@
 
 Console.WriteLine("Welcome to restest");
 
-Settings settings= new Settings();
-settings.checkSettings();
+if(args.Length == 0){
+    Console.WriteLine("No arguments, use H for help");
+} else if(args[0] == "D"){
+    Settings settings= new Settings();
+    settings.checkSettings();
 
-ResponseTest ResponseTest= new ResponseTest();
-await ResponseTest.TestResponseTimes(settings.getDuration(), settings.getUrl());
+    ResponseTest ResponseTest= new ResponseTest();
+    await ResponseTest.TestResponseTimes(settings.getDuration(), settings.getUrl());
+} else if(args[0] == "M"){
+    ResponseTest ResponseTest= new ResponseTest();
+    int MS = int.Parse(args[1]) * 1000;
+    string url = args[2];
+    await ResponseTest.TestResponseTimes(MS, url);
+} else if(args[0] == "H"){
+        Console.WriteLine("D = default settings");
+        Console.WriteLine("M = manual settings, time in seconds, url");
+        Console.WriteLine("H = help");
+} else{
+        Console.WriteLine("Invalid argument, use H for help");
+}
+
+
